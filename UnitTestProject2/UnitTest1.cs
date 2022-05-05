a8e7e8f9e753c0ed1fdcf418e103f3933167cf23
Nghi1 <nghivu201@gmail.com>
2022-05-05 22:30:02 +0700

    Add DataRow

--- UnitTestProject2/UnitTest1.cs
+++ UnitTestProject2/UnitTest1.cs
@@ -8,40 +8,57 @@ namespace UnitTestProject2
     public class UnitTest1
     {
         private Calculator c;
+        public TestContext TestContext { get; set; }
         [TestInitialize]
         public void SetUp()
         {
-            c = new Calculator(10, 5);
-        }
+          
+        }  
         [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",@".\Data\TestData.csv","TestData#csv",DataAccessMethod.Sequential)]
         public void TestMethod1()
         {
-            float expected, actual;
-            expected = 15;
+            float a,b, expected, actual;
+            a = float.Parse(TestContext.DataRow[0].ToString());
+            b=float.Parse(TestContext.DataRow[1].ToString());
+            expected = float.Parse(TestContext.DataRow[2].ToString());
+            c = new Calculator(a, b);
             actual = c.Exectute("+");
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @".\Data\TestData1.csv", "TestData1#csv", DataAccessMethod.Sequential)]
         public void TestMethod2()
         {
-            float expected, actual;
-            expected = 5;
+            float a, b, expected, actual;
+            a = float.Parse(TestContext.DataRow[0].ToString());
+            b = float.Parse(TestContext.DataRow[1].ToString());
+            expected = float.Parse(TestContext.DataRow[2].ToString());
+            c = new Calculator(a, b);
             actual = c.Exectute("-");
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @".\Data\TestData2.csv", "TestData2#csv", DataAccessMethod.Sequential)]
         public void TestMethod3()
         {
-            float expected, actual;
-            expected = 50;
+            float a, b, expected, actual;
+            a = float.Parse(TestContext.DataRow[0].ToString());
+            b = float.Parse(TestContext.DataRow[1].ToString());
+            expected = float.Parse(TestContext.DataRow[2].ToString());
+            c = new Calculator(a, b);
             actual = c.Exectute("*");
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @".\Data\TestData3.csv", "TestData3#csv", DataAccessMethod.Sequential)]
         public void TestMethod4()
         {
-            float expected, actual;
-            expected = 2;
+            float a, b, expected, actual;
+            a = float.Parse(TestContext.DataRow[0].ToString());
+            b = float.Parse(TestContext.DataRow[1].ToString());
+            expected = float.Parse(TestContext.DataRow[2].ToString());
+            c = new Calculator(a, b);
             actual = c.Exectute("/");
             Assert.AreEqual(expected, actual);
         }
